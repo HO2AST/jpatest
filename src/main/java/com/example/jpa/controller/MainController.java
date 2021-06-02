4f28cbbecc9cef6c24e36a50a6f2d0b6c0e2f852
ho2ast <ho2ast@innerwave.co.kr>
2021-06-02 16:12:43 +0900

    1번 디벨롭 수정

--- src/main/java/com/example/jpa/controller/MainController.java
+++ src/main/java/com/example/jpa/controller/MainController.java
@@ -30,7 +30,7 @@ public class MainController {
 	@PostMapping(path="/add") // Map ONLY POST Requests
 	public String addNewUser (@RequestParam String name, @RequestParam String email, Model model) {
     // @ResponseBody means the returned String is the response, not a view name
-    // @RequestParam means it is a parameter from the GET or POST request
+    // @RequestParam means it is a parameter from the GET or POST request12121212
 
 		User n = new User();
 		n.setName(name);
